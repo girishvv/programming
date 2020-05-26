0f0b0e5eedacb372aeca0944f71d214a183f340f
V V Girish <65946203+girishvv@users.noreply.github.com>
2020-05-26 15:15:57 +0530

    Update 001.contiguos_subarray_with_equal_0s_1s.py

--- Arrays & Strings/001.contiguos_subarray_with_equal_0s_1s.py
+++ Arrays & Strings/001.contiguos_subarray_with_equal_0s_1s.py
@@ -1,4 +1,8 @@
 
+# consider 0 as -1 and keep couting until count become 0
+# diffrence between indexes of last 0 seen and first 0 seen is the answer
+# dictionary is used to track the indices of count 0 seen
+
 class Solution:
     def findMaxLength(self, nums):
         if len(nums) == 0:
@@ -12,6 +16,10 @@ class Solution:
                 count -=1
             else :
                 count +=1
+            # if sum is 0, then note that index and add it to the
+            # dictionary if not already present in dict
+            # if present in dict then update the result with the diff of current  0 index
+            # and last seen 0 index
             if count == 0:
                 subarr = i + 1
             if count in hm:
