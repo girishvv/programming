f776c4366d3704cdc4a7a25de15a848ac4c8e40c
V V Girish <65946203+girishvv@users.noreply.github.com>
2020-05-26 17:52:50 +0530

    Add files via upload

---
+++ Dynamic Programming/001.BinomialCoefficient.py
@@ -0,0 +1,43 @@
+#from introduction.util.stop_watch import start_watch, stop_watch_print
+
+
+def binomial_coefficient(n, k):
+    if n == k or k == 0: return 1
+    return binomial_coefficient(n - 1, k - 1) + binomial_coefficient(n - 1, k)
+
+
+def binomial_coefficient_mem(n, k):
+    mem = [[0 for i in range(0, k + 1)] for i in range(0, n + 1)]
+    return binomial_coefficient_memoization(n, k, mem)
+
+
+def binomial_coefficient_memoization(n, k, mem):
+    if n == k or k == 0: return 1
+    if mem[n][k] != 0: return mem[n][k]
+    res = binomial_coefficient_memoization(n - 1, k - 1, mem) + binomial_coefficient_memoization(n - 1, k, mem)
+    mem[n][k] = res
+    return res
+
+
+def binomial_coefficient_dp(n, k):
+    dp = [[0 for i in range(0, k + 1)] for i in range(0, n + 1)]
+    for i in range(0, n + 1):
+        dp[i][0] = 1
+        if i <= k:
+            dp[i][i] = 1
+            
+    for i in range(1, n + 1):
+        for j in range(1, k + 1):
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+    return dp[n][k]
+
+N=4
+K=3
+
+print(binomial_coefficient(N,K))
+
+
+print(binomial_coefficient_mem(N,K))
+
+
+print(binomial_coefficient_dp(N,K))
